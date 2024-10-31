643d2382d06e918c671aafdf2f0826e6a8ab4cb1
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-10-31 23:06:06 +0200

    modifying and writing js codes to payment componet

---
+++ src/Features/PaymentPage/PaymentGateway.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Card, CardContent, CardTitle } from './kard';
+import { Button } from './button';
+import { Input } from './Input';
+import { Store, MapPin } from 'lucide-react';
