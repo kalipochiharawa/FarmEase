4b228216d2ce10dbf1f4b220dea624fc200e65fe
“kidlocchikapagit config --global user.name “kidlocchikapa <bed-com-48-44@unima.ac.mw>
2024-11-11 20:01:57 +0200

    Save changes before switching branches

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import Navbar from "./Features/LandingPage/MainHead.jsx";
-import CategorySlider from "./Features/LandingPage/Subcomponents/CategoriesCards.jsx";
-import LandingPage from "./Features/LandingPage/Subcomponents/LandingPage.jsx";
-import ProductGrid from "./Features/LandingPage/Subcomponents/ProductsCard.jsx";
-import RegisterSection from "./Features/LandingPage/Subcomponents/RegisterAdd.jsx";
-import ProductMarket from "./Features/LandingPage/Subcomponents/Marketshow.jsx";
-import TestimonialSlider from "./Features/LandingPage/Subcomponents/Testimonial.jsx";
-import MarketplaceSection from "./Features/LandingPage/Subcomponents/MarketPlace.jsx";
-import BenefitsProcess from "./Features/LandingPage/Subcomponents/Benefits.jsx";
-import Footer from './Features/LandingPage/Subcomponents/Footer.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPageLayout from './LandingPageLayout';
+
 function App() {
   return (
-    <div >
-      <Navbar/>
-      <LandingPage/>
-      <CategorySlider/>
-      <ProductGrid/>
-      <RegisterSection/>
-      <ProductMarket/>
-      <MarketplaceSection/>
-      <TestimonialSlider/>
-      <BenefitsProcess/>
-      <Footer/>
-     
-   
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPageLayout />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
