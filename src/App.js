1ea0e4bd7867ffb7c698cb247a513ed5cff92006
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-11-12 19:52:59 +0200

    landingsite

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import Navbar from "./Features/LandingPage/MainHead.jsx";
-import CategorySlider from "./Features/LandingPage/Subcomponents/CategoriesCards.jsx";
-import LandingPage from "./Features/LandingPage/Subcomponents/LandingPage.jsx";
-import ProductGrid from "./Features/LandingPage/Subcomponents/ProductsCard.jsx";
-import RegisterSection from "./Features/LandingPage/Subcomponents/RegisterAdd.jsx";
-import ProductMarket from "./Features/LandingPage/Subcomponents/Marketshow.jsx";
-import TestimonialSlider from "./Features/LandingPage/Subcomponents/Testimonial.jsx";
-import MarketplaceSection from "./Features/LandingPage/Subcomponents/MarketPlace.jsx";
-import BenefitsProcess from "./Features/LandingPage/Subcomponents/Benefits.jsx";
-import Footer from './Features/LandingPage/Subcomponents/Footer.jsx';
+// File: App.js
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./Features/LandingPage/LandingSite"; 
+
 function App() {
   return (
-    <div >
-      <Navbar/>
-      <LandingPage/>
-      <CategorySlider/>
-      <ProductGrid/>
-      <RegisterSection/>
-      <ProductMarket/>
-      <MarketplaceSection/>
-      <TestimonialSlider/>
-      <BenefitsProcess/>
-      <Footer/>
-     
-   
+    <div>
+      <HomePage />
     </div>
   );
-};
+}
 
 export default App;
