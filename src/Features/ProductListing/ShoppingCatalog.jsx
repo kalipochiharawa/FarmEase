fcca12f347c697b91e9e1c050137a5bca24fa25d
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-11-12 21:40:39 +0200

    Routing and Modification phase 1.1

--- src/Features/ProductListing/ShoppingCatalog.jsx
+++ src/Features/ProductListing/ShoppingCatalog.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { Search, MapPin, Store } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const ProductListing = () => {
   const navigate = useNavigate();
@@ -272,12 +273,14 @@ const ProductListing = () => {
                     <span className="font-semibold">
                       Price : K{product.price} per Kg
                     </span>
+                    <Link to="/PayementPage">
                     <button
                       onClick={() => handleBuyNow(product)}
                       className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 transition"
                     >
                       Buy Now
                     </button>
+                    </Link>
                   </div>
                 </div>
               </div>
