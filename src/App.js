f97d918cc6e8c99968db83e110cd63bc5b418c5a
blessingsChithonje <bed-com-28-21@unima.ac.mw>
2024-11-15 14:57:03 +0000

    editing component profile management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPageLayout from './Features/LandingPage/LandingpageLayout';
+import MyProfile from './Features/MyProfile/MyProfile';
 
 function App() {
   return (
+    <>
     <LandingPageLayout/>
+    
+    </>
   );
 }
 
