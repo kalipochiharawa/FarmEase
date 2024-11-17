b4d5c255f34892f172155c5aa4d8df40cc11c531
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-11-17 22:45:26 +0200

    updating the app.js for testing new feature

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React from "react";
-import { CheckCircle } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPageLayout from './Features/LandingPage/LandingpageLayout';
+import Registering from './Features/UserRegistration/Register';
+import SignUp from './Features/UserRegistration/Login';
+import ProductListing from './Features/ProductListing/ShoppingCatalog';
+import AddProductForm from './Features/AddProducts/Addproduct';
+import ProfileManagement from './Features/MyProfile/MyProfile';
+import PaymentGateway from './Features/PayementGateway/PayementPage';
 
-const TransactionSuccess = () => {
-  const navigate = useNavigate();
-
-  const handleBackToHome = () => {
-    navigate("/"); // Redirecting the user back to the homepage
-  };
-
+function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4">
-      <div className="text-center max-w-lg">
-        <CheckCircle className="w-28 h-28 text-green-500 mx-auto mb-4" />
-        <h1 className="text-3xl font-bold mb-2 text-gray-800">
-          Payment Successful!
-        </h1>
-        <p className="text-gray-600 mb-6 text-base">
-          Thank you for your purchase. Your transaction was completed
-          successfully.
-        </p>
-        <button
-          onClick={handleBackToHome}
-          className="bg-green-600 text-white px-6 py-3 rounded-lg text-lg font-semibold shadow hover:bg-green-700 transition"
-        >
-          Back to Home
-        </button>
-        <footer className="mt-6 text-sm text-gray-500">
-          Developed by{" "}
-          <span className="font-bold text-green-700">QuantiqueSystems</span>
-        </footer>
-      </div>
+    <div className="App">
+      {/* Only one Router at the root level */}
+      <Routes>
+        <Route path="/Login" element={<SignUp />} />
+      </Routes>
     </div>
   );
-};
+}
 
-export default TransactionSuccess;
+export default App;
