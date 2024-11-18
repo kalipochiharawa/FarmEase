0411611f746d2f26c68af5b977ac7ca178d93bcb
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-11-18 09:45:28 +0200

    Testing FRONTEND WITH BACKEND

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AddProductForm from './Features/ProductListing/addingProducts';
+import ProductListing from './Features/ProductListing/ShoppingCatalog';
 
 function App() {
   return (
     <div className="App">
       {/* Only one Router at the root level */}
       <Routes>
-        <Route path="/" element={<AddProductForm />} />
+        <Route path="/" element={<ProductListing />} />
       </Routes>
     </div>
   );
