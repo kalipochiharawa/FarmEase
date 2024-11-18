599db3049a12cc3bea2b212e9114227584de3cea
MCHIHANA <bsc-com-01-21@unima.ac.mw>
2024-11-18 09:23:36 +0200

    commiting files togithub

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LandingPageLayout from './Features/LandingPage/LandingpageLayout';
-import Registering from './Features/UserRegistration/Register';
-import SignUp from './Features/UserRegistration/Login';
-import ProductListing from './Features/ProductListing/ShoppingCatalog';
-import AddProductForm from './Features/AddProducts/Addproduct';
-import ProfileManagement from './Features/MyProfile/MyProfile';
-import PaymentGateway from './Features/PayementGateway/PayementPage';
+import AddProductForm from './Features/ProductListing/addingProducts';
 
 function App() {
   return (
     <div className="App">
       {/* Only one Router at the root level */}
       <Routes>
-        <Route path="/Login" element={<SignUp />} />
+        <Route path="/" element={<AddProductForm />} />
       </Routes>
     </div>
   );
