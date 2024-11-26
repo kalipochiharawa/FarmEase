06b89bcc51f57f001298e5eab742f86805df99f2
kidloc chikapa <bed-com-48-22@unima.ac.mw>
2024-11-27 04:10:10 +0500

    added font to a cormfimation page

--- src/Features/PayementGateway/Confirmed.jsx
+++ src/Features/PayementGateway/Confirmed.jsx
@@ -10,7 +10,7 @@ const TransactionSuccess = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-green-200 via-green-400 to-green-600">
+    <div className="min-h-screen font-poppins flex flex-col justify-center items-center bg-gradient-to-r from-green-200 via-green-400 to-green-600">
       <div className="bg-white rounded-lg shadow-xl p-8 text-center max-w-md">
         <CheckCircle className="w-28 h-28 text-green-600 mx-auto mb-4" />
         <h1 className="text-4xl font-extrabold mb-2 text-gray-800">
